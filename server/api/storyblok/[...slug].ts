d292e92760fbc416fc479e29f8ecf3c2d4ec99d2
sebbejohansson <sebbe.johansson1995@gmail.com>
2023-02-15 23:06:28 +0100

    adds support for a persistant storyblok api instance on the server api.

--- server/api/storyblok/[...slug].ts
+++ server/api/storyblok/[...slug].ts
@@ -1,17 +1,26 @@
 import StoryBlokClient from 'storyblok-js-client';
 
-export default defineEventHandler((event) => {
-  const runtimeConfig = useRuntimeConfig();
-  const storyblokConfig = runtimeConfig.public.storyblok as any; // Since not everything exists properly typed.
-  const storyblokApiToken = storyblokConfig?.accessToken;
-  const storyblokApiOptions = storyblokConfig?.apiOptions;
+let storyblokApiInstance = new StoryBlokClient({});
+let boolean = false;
 
-  const storyblokApiInstance = new StoryBlokClient(
-    {
-      ...storyblokApiOptions,
-      accessToken: storyblokApiToken,
-    },
-  );
+export default defineEventHandler((event) => {
+  if (!boolean) {
+    console.log('boolean is false which means we dont have a proper storyblok api instance');
+    const runtimeConfig = useRuntimeConfig();
+    const storyblokConfig = runtimeConfig.public.storyblok as any; // Since not everything exists properly typed.
+    const storyblokApiToken = storyblokConfig?.accessToken;
+    const storyblokApiOptions = storyblokConfig?.apiOptions;
+
+    storyblokApiInstance = new StoryBlokClient(
+      {
+        ...storyblokApiOptions,
+        accessToken: storyblokApiToken,
+      },
+    );
+  } else {
+    console.log('boolean is true which means we have a proper storyblok api instance');
+  }
+  boolean = true;
 
   const slug = event.context.params?.slug;
   const query = getQuery(event);
